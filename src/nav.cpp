d236f9657efeafd9361e36adf552b1e9bb63fad4
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-23 12:18:10 +0530

    Seems like I neded to modify the nav.cpp a bit still working on it now

--- src/nav.cpp
+++ src/nav.cpp
@@ -1,11 +1,21 @@
-#include "../include/nav.h"
+#include "nav.h"
 
-Nav::Nav(QWidget *parent)
-    : QWidget(parent), ui(new Ui::Nav) {
-    ui->setupUi(this);  
+Nav::Nav(QWidget *parent) : QWidget(parent), ui(new Ui::Nav) {
+    ui->setupUi(this);
+
+    ui->welcome->setProperty("active", true);
+
+    ui->welcome->style()->polish(ui->container_button);
+
+    connect(ui->container_button, &QPushButton::clicked, [=]() {
+        ui->container_button->setProperty("active", true);
+        ui->vm_button->setProperty("active", false);
+        ui->container_button->style()->polish(ui->container_button);
+        ui->vm_button->style()->polish(ui->vm_button);
+        emit buttonClicked("containers");
+    });
 }
 
 Nav::~Nav() {
-    delete ui;  
+    delete ui;
 }
-
