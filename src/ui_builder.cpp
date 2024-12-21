ddc3e0a5362c21470ed597614710220fd95aebb8
arungeorgesaji <arungeorgesaji@gmail.com>
2024-12-21 23:33:50 +0530

    added the headinglabel to the welcome ui and fixed any issues

--- src/ui_builder.cpp
+++ src/ui_builder.cpp
@@ -9,7 +9,7 @@ Nav* createNav(QWidget* parent) {
 
 QScrollArea* createWelcomePage() {
     Welcome* welcome = new Welcome();  
-    welcome->setFixedSize(200, 800);
+    welcome->setFixedSize(900, 800);
     loadWidgetStyleSheet(welcome, ":/styles/styles/welcome.qss");
     
     QWidget* welcomePage = new QWidget();
@@ -28,7 +28,7 @@ QScrollArea* createWelcomePage() {
 
 QScrollArea* createLocalesPage() {
     Locales* locales = new Locales();  
-    locales->setFixedSize(200, 800);
+    locales->setFixedSize(900, 800);
     loadWidgetStyleSheet(locales, ":/styles/styles/locales.qss");
     
     QWidget* localesPage = new QWidget();
@@ -47,7 +47,7 @@ QScrollArea* createLocalesPage() {
 
 QScrollArea* createDiskSetupPage() {
     DiskSetup* diskSetup = new DiskSetup();  
-    diskSetup->setFixedSize(200, 800);
+    diskSetup->setFixedSize(900, 800);
     loadWidgetStyleSheet(diskSetup, ":/styles/styles/disk_setup.qss");
     
     QWidget* diskSetupPage = new QWidget();
@@ -66,7 +66,7 @@ QScrollArea* createDiskSetupPage() {
 
 QScrollArea* createUsersPage() {
     Users* users = new Users();  
-    users->setFixedSize(200, 800);
+    users->setFixedSize(900, 800);
     loadWidgetStyleSheet(users, ":/styles/styles/users.qss");
     
     QWidget* usersPage = new QWidget();
@@ -85,7 +85,7 @@ QScrollArea* createUsersPage() {
 
 QScrollArea* createNetworkPage() {
     Network* network = new Network();  
-    network->setFixedSize(200, 800);
+    network->setFixedSize(900, 800);
     loadWidgetStyleSheet(network, ":/styles/styles/network.qss");
     
     QWidget* networkPage = new QWidget();
@@ -104,7 +104,7 @@ QScrollArea* createNetworkPage() {
 
 QScrollArea* createPackagesPage() {
     Packages* packages = new Packages();  
-    packages->setFixedSize(200, 800);
+    packages->setFixedSize(900, 800);
     loadWidgetStyleSheet(packages, ":/styles/styles/packages.qss");
     
     QWidget* packagesPage = new QWidget();
@@ -123,7 +123,7 @@ QScrollArea* createPackagesPage() {
 
 QScrollArea* createInstallPage() {
     Install* install = new Install();  
-    install->setFixedSize(200, 800);
+    install->setFixedSize(900, 800);
     loadWidgetStyleSheet(install, ":/styles/styles/install.qss");
     
     QWidget* installPage = new QWidget();
