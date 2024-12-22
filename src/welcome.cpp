d988f80cb721f36402deaf8e997bce8a775dc5d3
arungeorgesaji <arungeorgesaji@gmail.com>
2024-12-22 17:06:55 +0530

    Made the languages be added dynamically from cpp

--- src/welcome.cpp
+++ src/welcome.cpp
@@ -2,6 +2,9 @@
 
 Welcome::Welcome(QWidget *parent) : QMainWindow(parent), ui(new Ui::Welcome) {
     ui->setupUi(this);
+
+    QStringList languages = {"English", "Spanish", "French", "German", "Italian"};
+    ui->languageComboBox->addItems(languages);
 }
 
 Welcome::~Welcome() {
