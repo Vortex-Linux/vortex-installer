6a208a2dce33e3da769063c856f07c72453bc4e2
arungeorgesaji <arungeorgesaji@gmail.com>
2025-02-08 18:09:11 +0530

    Added the basic ui for locales now need to actually do stuff

--- src/welcome.cpp
+++ src/welcome.cpp
@@ -4,6 +4,7 @@ Welcome::Welcome(QWidget *parent) : QMainWindow(parent), ui(new Ui::Welcome) {
     ui->setupUi(this);
 
     QStringList languages = {"English", "Spanish", "French", "German", "Italian"};
+    languages.sort();
     ui->languageComboBox->addItems(languages);
 }
 
